b228c1b6e6450e9adc5d84ee6ffa7a2baa1aceeb
Chanon Treemeth <v_chanont@ctlsd.set>
2020-02-22 21:21:09 +0700

    Adjust schema in movie

--- api/models/movie.js
+++ api/models/movie.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const movieSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name:{type:String},
-    UrlImage:{type:String},
-    create:{type:String},
-    rating:{type:String}
+    urlImage:{type:String},
+    create:{type:Number},
+    rating:{type:Number}
 })
 
 module.exports = mongoose.model('movies', movieSchema)
