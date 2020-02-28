3e5614d7c3a004379c722af9ed067ae8f2068863
Chanon Treemeth <v_chanont@ctlsd.set>
2020-02-28 23:16:02 +0700

    Adjust port

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http')
 const app = require('./app')
 
-const port = process.env.port || 3000
+const port = process.env.port || 5000
 
 const server = http.createServer(app)
 
 server.listen(port,()=>{
-    console.log("server start at prot 3000")
+    console.log("server start at prot 5000")
 })
