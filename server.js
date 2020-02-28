f3f74bbde1e8f4497decf5708ae3e34d4365827c
Chanon Treemeth <v_chanont@ctlsd.set>
2020-02-28 23:44:50 +0700

    update Server

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const http = require('http')
 const app = require('./app')
 
 const port = process.env.port || 5000
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 const server = http.createServer(app)
 
-server.listen(port,()=>{
+server.listen(port,server_host,()=>{
     console.log("server start at prot 5000")
 })
